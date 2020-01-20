ca3e97b8fa7870c7d498c78ffb3c7c1afdcaa94f
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-01-20 22:08:56 +0300

    preceptor

---
+++ story_content/user.js
@@ -0,0 +1,54 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5dB43cAmx8D":
+        Script1();
+        break;
+      case "5eYAnqdfVed":
+        Script2();
+        break;
+      case "5eERVfpiRNU":
+        Script3();
+        break;
+      case "5cZnITogEd9":
+        Script4();
+        break;
+      case "6P5JwKwO004":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=month + "/" + day + "/" + year
+
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script3()
+{
+  $("#tab-customlink").show();
+}
+
+function Script4()
+{
+  $("#tab-customlink").show();
+}
+
+function Script5()
+{
+  window.print();
+}
+
